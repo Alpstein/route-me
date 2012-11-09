8ffe90c6808ce29652de8a7c684f7e64307f1e4e
Tracy Harton <tracy@amphibioustech.com>
2012-11-09 11:24:04 -0800

    Enhanced RMCircle with inner radius (for an annulus) and start/end for an arc

--- MapView/Map/RMCircle.h
+++ MapView/Map/RMCircle.h
@@ -31,6 +31,7 @@
 
 @class RMMapView;
 
+/** An RMCircle is used to represent a perfect circle shape on a map view. An RMCircle changes visible size in response to map zooms in order to consistently represent coverage of the same geographic area. */
 @interface RMCircle : RMMapLayer
 {
 	RMMapView *mapView;
@@ -45,12 +46,37 @@
 	CGMutablePathRef circlePath;
 }
 
+/** @name Accessing Drawing Properties */
+
+/** The circle's underlying shape layer. */
 @property (nonatomic, retain) CAShapeLayer *shapeLayer;
+
+/** The circle's line color. Defaults to black. */
 @property (nonatomic, retain) UIColor *lineColor;
+
+/** The circle's fill color. Defaults to blue. */
 @property (nonatomic, retain) UIColor *fillColor;
+
+/** The radius of the circle in projected meters. Regardless of map zoom, the circle will change visible size to continously represent this radius on the map. */
+@property (nonatomic, assign) CGFloat innerRadiusInMeters;
 @property (nonatomic, assign) CGFloat radiusInMeters;
+@property (nonatomic, assign) CGFloat arcStart;
+@property (nonatomic, assign) CGFloat arcEnd;
+@property (nonatomic, assign) BOOL fullCircle;
+
+
+
+/** The circle's line width. Defaults to 10.0. */
 @property (nonatomic, assign) CGFloat lineWidthInPixels;
 
+/** @name Creating Circle Objects */
+
+/** Initializes and returns a newly allocated RMCircle for the specified map view.
+*   @param aMapView The map view the shape should be drawn on.
+*   @param newRadiusInMeters The radius of the circle object in projected meters. Regardless of map zoom, the circle will change visible size to continously represent this radius on the map. */
 - (id)initWithView:(RMMapView *)aMapView radiusInMeters:(CGFloat)newRadiusInMeters;
+
+- (id)initWithView:(RMMapView *)aMapView innerRadiusInMeters:(CGFloat)_innerRadiusInMeters radiusInMeters:(CGFloat)_radiusInMeters arcStart:(CGFloat) _arcStart arcEnd:(CGFloat)_arcEnd fullCircle:(BOOL)_fullCircle;
+
 
 @end
